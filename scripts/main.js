70c4f6f58ad1b7ad1d055bc5f6953542abf45a31
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-05-14 22:17:00 +0300

    Fix panelsnap

--- scripts/main.js
+++ scripts/main.js
@@ -14,48 +14,21 @@ var Portfolio = (function($) {
         });
     }
 
-    function viewButtons() {
-        $landing = $('#landing');
-        $portfolio = $('#portfolio');
-
-        $('body').click(function() {
-            $portfolio.removeClass('hidden');
-            $('body').animate(
-                {scrollTop: $(window).height()},
-                1000,
-                'swing',
-                function() {
-                    $landing.addClass('hidden');
-                }
-            );
-        });
-    }
-
     function initPanelSnap() {
         var options = {
-            $menu: false,
-            menuSelector: 'a',
-            panelSelector: 'section',
-            namespace: '.panelSnap',
-            onSnapStart: function(){},
-            onSnapFinish: function(){},
-            onActivate: function(){},
-            directionThreshold: 50,
-            slideSpeed: 200,
             keyboardNavigation: {
-                enabled: trye,
+                enabled: true,
                 nextPanelKey: 40,
                 previousPanelKey: 38,
                 wrapAround: false
             }
         };
 
-        $('.sections').panelSnap(options);
+        $('body').panelSnap(options);
     }
 
     function init() {
         //setTimeout(animateBlurBackground, 500);
-        viewButtons();
         initPanelSnap();
     }
 
